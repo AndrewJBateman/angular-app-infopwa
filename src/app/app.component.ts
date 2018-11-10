6383c97225f12b4bd61c2a328e60a73ac1fe8970
Andrew Bateman <gomezbateman@yahoo.com>
2018-11-10 21:00:34 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { constructDependencies } from '@angular/core/src/di/reflective_provider';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app-infoPwa';
+
+	update: boolean = false;
+
+	constructor(updates: SwUpdate) {
+		updates.available.subscribe(event => {
+
+			this.update = true;
+		})
+	}
+
 }
+
+
