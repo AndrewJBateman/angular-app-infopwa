85cb608efebad51db9167feac4a6b9746f956f11
AndrewJBateman <gomezbateman@yahoo.com>
2022-06-27 12:18:23 +0200

    feat: add pwa sw update service

---
+++ src/app/services/pwa.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { SwUpdate, VersionReadyEvent } from "@angular/service-worker";
+import { filter, map } from "rxjs/operators";
+@Injectable({
+  providedIn: "root",
+})
+export class UpdateService {
+  constructor(private updates: SwUpdate) {}
+
+  public checkForUpdates() {
+    this.updates.versionUpdates
+      .pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === "VERSION_READY"),
+        map((evt) => ({
+          type: "UPDATE_AVAILABLE",
+          current: evt.currentVersion,
+          available: evt.latestVersion,
+        }))
+      )
+      .subscribe((event) => {
+        console.log(event);
+        if (event.type === "UPDATE_AVAILABLE") {
+          this.promptUser();
+        }
+      });
+  }
+
+  private promptUser() {
+    this.updates.activateUpdate().then(() => {
+      window.location.reload();
+    });
+  }
+}
